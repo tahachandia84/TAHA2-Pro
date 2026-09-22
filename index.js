064f1d1694fe478298e078ac921135064be7935f
Taha khan <tahachandia84@gmail.com>
2026-09-23 00:43:01 +0500

    Change spawned script from EryXenX.js to Taha.js

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ const { spawn } = require("child_process");
 const log = require("./logger/log.js");
 
 function startProject() {
-	const child = spawn("node", ["EryXenX.js"], {
+	const child = spawn("node", ["Taha.js"], {
 		cwd: __dirname,
 		stdio: "inherit",
 		shell: true
